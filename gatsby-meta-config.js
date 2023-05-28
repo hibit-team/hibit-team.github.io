e148e07b123347b1c0a73900efa89e7a402a3c55
Junyong Moon <fancy.junyongmoon@gmail.com>
2023-05-28 22:01:12 +0900

    github, instagram url 에러 수정

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,18 +1,18 @@
 module.exports = {
   title: `hibit`,
   description: `Blog posted about ...`,
-  author: `hibit-team`,
+  author: `devFancy`,
   introduction: `히빗팀 기술 블로그입니다.`,
   siteUrl: `https://hibit-team.github.io/`, // Your blog site url
   social: {
-    github: `https://hibit-team.github.io/`, // Your GitHub account
-    instagram: `https://www.instagram.com/hibit.co.kr/`, // Your Instagram account
+    github: `hibit-team.github.io/`, // Your GitHub account
+    instagram: `hibit.co.kr/`, // Your Instagram account
   },
   icon: `content/assets/felog.png`, // Add your favicon
   keywords: [`blog`],
   comment: {
     disqusShortName: '', // Your disqus-short-name. check disqus.com.
-    utterances: 'https://hibit-team.github.io/', // Your repository for archive comment
+    utterances: 'hibit-team/hibit-team.github.io', // Your repository for archive comment
   },
   configs: {
     countOfInitialPost: 10, // Config your initial count of post
